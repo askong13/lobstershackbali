c80802f3d8aec7b2c8cdfe068056ac4ff6a60e1e
Jamal Abdullah <62120216+askong13@users.noreply.github.com>
2025-07-28 02:29:12 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,7 +88,7 @@ const renderMenu = () => {
                 <div class="ls-card-actions">
                     <button class="ls-btn ls-view-details-btn"><i class="fas fa-eye"></i> <span data-lang-key="view_details_btn">View Details</span></button>
                     ${p.gofood_link ? `<a href="${p.gofood_link}" target="_blank" rel="noopener noreferrer" class="ls-order-link" aria-label="Order on GoFood"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF4G3lRAltCydaksfc29WU0fH7mJrnweaDYLqBrCw33Vxb6QfzKD87ANo&s=10" alt="GoFood Logo" style="width:50px; height:50px;"></a>` : ''}
-                    ${p.grabfood_link ? `<a href="${p.grabfood_link}" target="_blank" rel="noopener noreferrer" class="ls-order-link" aria-label="Order on GrabFood"><img src="https://i.ibb.co/qDWMzWd/grab-logo-48x48.png" alt="GrabFood Logo" style="width:24px; height:24px;"></a>` : ''}
+                    ${p.grabfood_link ? `<a href="${p.grabfood_link}" target="_blank" rel="noopener noreferrer" class="ls-order-link" aria-label="Order on GrabFood"><img src="https://iconlogovector.com/uploads/images/2023/11/lg-655d63568932b-grab-food.png" alt="GrabFood Logo" style="width:50px; height:50px;"></a>` : ''}
                 </div>
             </div> 
         </div>`;
